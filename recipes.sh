ea187a6da9f2d44330247876461382b222a7b4e4
Jesse Mu <jlm95@cam.ac.uk>
2018-04-13 18:45:48 +0000

    Update save location + recipes

--- recipes.sh
+++ recipes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 train_definition_model.py --summary_dir ./logs/default_params/ --num_epochs 100
+python3 train_definition_model.py --summary_dir ./logs/default_params/ --save_dir ./saves/default_params/ --num_epochs 100
---
+++ saves/README.md
@@ -0,0 +1,1 @@
+# Placeholder for tensorflow save models
