c85a5a5d6e3bd62ef534a2abe7d552f272536612
Jesse Mu <jlm95@cam.ac.uk>
2018-04-13 20:08:36 +0000

    Update recipes

--- recipes.sh
+++ recipes.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 python3 train_definition_model.py --summary_dir ./logs/default_params/ --save_dir ./saves/default_params/ --num_epochs 100
+
+python3 train_definition_model.py --optimizer rmsprop --summary_dir ./logs/rmsprop/ --save_dir ./saves/rmsprop/ --num_epochs 100
+
+python3 train_definition_model.py --optimizer gradientdescent --summary_dir ./logs/gradientdescent/ --save_dir ./saves/gradientdescent/ --num_epochs 100
